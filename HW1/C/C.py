20953c00c4d31906eb78d4766877606c9e10f47e
DianaKartinina <dina-1701@yandex.ru>
2022-03-06 12:30:26 +0300

    added solution C.py

--- HW1/C/C.py
+++ HW1/C/C.py
@@ -1,3 +1,5 @@
 def solution(n, k):
-    ### YOUR CODE HERE ###
+    shkol = int(input('Сколько наличиствут школьников? '))
+    yabl = int(input('Сколько яблок надо разделить между этими школьниками? '))
+    print(yabl // shkol , '\n' , yabl % shkol)
     return
